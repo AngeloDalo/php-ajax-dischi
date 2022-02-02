7a79b8e46e6d59e9c8f544693199db83dd5d8541
AngeloDalo <angelo251158@gmail.com>
2022-02-02 15:44:16 +0100

    base index-api

--- index-ajax.php
+++ index-ajax.php
@@ -1,1 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+  <title>PHP Dischi</title>
+</head>
+
+<body>
+  <!-- header -->
+  <?php include_once __DIR__ . '/partials/header.php' ?>
+
+  <!-- main -->
+
+  <!-- footer -->
+  <?php include_once __DIR__ . '/partials/footer.php' ?>
+  
+  <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+  <script src="js/script.js"></script>
+</body>
+
+</html>
